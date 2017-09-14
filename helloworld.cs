ef13fd7d66845030c156dc8fb6493e3f02bb8c56
devonhackley <devonhackley@gmail.com>
2017-09-14 10:26:32 -0700

    took a minute but first csharp program works

---
+++ helloworld.cs
@@ -0,0 +1,13 @@
+using System;
+namespace HelloWorldApplication
+{
+  class HelloWorld
+  {
+    static void Main(string[] args)
+    {
+      /* hello world program */
+      Console.WriteLine("Hello World this is C#");
+      Console.ReadKey();
+    }
+  }
+}
